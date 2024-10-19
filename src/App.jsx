6d77345d9434f62e68bf831e22ac1751cc3f51d5
KittoSanon <kittosanon@gmail.com>
2024-10-19 16:57:21 +0700

    edit event

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./pages/HOME/home";
+
+function App() {
+  return (
+  <div> 
+    <Home/>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/pages/HOME/home.jsx
@@ -0,0 +1,9 @@
+import "./home.scss";
+
+const Home = () => {
+    return (
+        <div>I want to sleep la</div>
+    )
+}
+
+export default Home
