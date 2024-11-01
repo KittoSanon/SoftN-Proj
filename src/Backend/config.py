4a7377982f57c72dfa0ffdb22e4f63763558fd8c
KittoSanon <kittosanon@gmail.com>
2024-11-01 16:52:56 +0700

    Delete src/Backend directory

--- src/Backend/config.py
+++
@@ -1,6 +0,0 @@
-# Define connection parameters
-host = "localhost"      # or your server's IP address
-port = "5432"
-user = "postgres"       # replace with your PostgreSQL username
-password = "1234"       # replace with your PostgreSQL password
-database_name = "postgres"  # replace with the name of the database you want to create
